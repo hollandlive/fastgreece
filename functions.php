23348ea921c723a99ef199b44b1595dc14e75b91
Artem Aksenov <dutchnewsru@gmail.com>
2020-05-13 08:47:38 +0300

    child theme

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 
-function direo_child_enqueue_parent_styles() {
+function fastgreece_enqueue_parent_styles() {
     if (is_rtl()) {
-        wp_style_add_data('direo-style', 'rtl', 'replace');
+        wp_style_add_data('fastgreece', 'rtl', 'replace');
     } else {
         wp_enqueue_style( 'parent-style', get_parent_theme_file_uri('/style.css') );
     }
 }
-add_action( 'wp_enqueue_scripts', 'direo_child_enqueue_parent_styles' );
+add_action( 'wp_enqueue_scripts', 'fastgreece_enqueue_parent_styles' );
